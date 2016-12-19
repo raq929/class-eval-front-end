3593461465e521e7dae856a4d25af789a9a19541
racheltstevens <racheltstevens@gmail.com>
2016-12-19 10:29:00 -0500

    Roster list is properly displaying
    No individual rosters

---
+++ app/components/roster-list/student/component.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  tagName: 'li',
+  actions: {
+
+    // delete () {
+    //   this.sendAction('delete', this.get('item'));
+    // },
+  },
+});
---
+++ app/rosters/roster/route.js
@@ -0,0 +1,24 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model (params) {
+    return this.get('store').findRecord('roster', params.roster_id);
+  },
+
+  actions: {
+    // toggleItemDone (item) {
+    //   item.toggleProperty('done');
+    //   return item.save();
+    // },
+    //
+    // deleteItem (item) {
+    //   return item.destroyRecord();
+    // },
+    //
+    // createItem (data) {
+    //   let item = this.get('store').createRecord('item', data);
+    //   return item.save();
+    // },
+
+  },
+});
